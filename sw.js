3aca1869429ee35c1c94ae572377c992fb74d48c
teru-kuma <162723208+teru-kuma@users.noreply.github.com>
2026-07-22 01:16:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "voicelab-v4";
+const CACHE = "voicelab-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
